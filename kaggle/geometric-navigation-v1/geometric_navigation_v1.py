10b54c179d8bee0a019686eb2bdf23562207335e
Diari <diari.shareef@gmail.com>
2026-08-03 09:45:56 +0200

    Repin V1 kernel after preflight fix

--- kaggle/geometric-navigation-v1/geometric_navigation_v1.py
+++ kaggle/geometric-navigation-v1/geometric_navigation_v1.py
@@ -15,7 +15,7 @@ import traceback
 
 
 REPOSITORY_URL = "https://github.com/Diari/one-layer-deeper.git"
-GIT_COMMIT = "e5945d7ab5e89c86964540ada0a67cfe128f23bb"
+GIT_COMMIT = "d2428733734c6621718fcb73cde6b54229d0faab"
 EXPERIMENT_SEQUENCE = ("e1", "e2", "e5", "e3", "e4")
 STOP_AFTER_DATASET = "e1"
 PRIMARY_VARIANTS = ("control", "full")
